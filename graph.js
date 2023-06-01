8a39e0b22184012bf555706880d3b8ce7a1d835f
Alyssa Denary <alyssa.denary@gmail.com>
2023-06-01 14:37:51 -0400

    addEdge

--- graph.js
+++ graph.js
@@ -28,7 +28,10 @@ class Graph {
    }
 
   /** add edge between vertices v1,v2 */
-  addEdge(v1, v2) { }
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   /** remove edge between vertices v1,v2 */
   removeEdge(v1, v2) { }
